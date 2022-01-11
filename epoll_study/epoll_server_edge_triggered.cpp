a5857d57128cd19b6c5e48111af05e651a419d03
betteryjs <1307534519@qq.com>
2022-01-11 22:49:50 +0800

    fix epoll_server_edge_triggered.cpp

--- epoll_study/epoll_server_edge_triggered.cpp
+++ epoll_study/epoll_server_edge_triggered.cpp
@@ -2,22 +2,21 @@
 // Created by yjs on 2022/1/8.
 //
 
-//#include <iostream>
-//#include <arpa/inet.h>
+#include <iostream>
+#include <arpa/inet.h>
 #include "wrap.h"
 #include <sys/epoll.h>
-//#include <string>
-//#include <netinet/in.h>
-//#include <unordered_map>
-#include <bits/stdc++.h>
+#include <string>
+#include <netinet/in.h>
+#include <unordered_map>
 #include <fcntl.h>
 
 using namespace std;
 
 
-const short Port = 8000;
+const int ServePort = 8000;
 const char *ServerIp = "192.168.2.249";
-const short EvsSize = 1024;
+const int EpollEvsSize = 1024;
 const short ReadBufferSize = 4;
 const short EpollWaitTimeout = -1;
 
@@ -26,34 +25,49 @@ int main() {
 
     // 创建套结字
     int listen_fd;
-    listen_fd = tcp4bind(Port, ServerIp);
+    listen_fd = tcp4bind(ServePort, ServerIp);
+    // 监听套结字
     Listen(listen_fd, 128);
-
-
-
     // 创建红黑树
+    int epoll_fd = epoll_create(1);
 
-    int epfd = epoll_create(1);
+    if (epoll_fd == -1) {
+        cout << "create epollfd error." << endl;
+        close(listen_fd);
+        return -1;
+    }
 
     // 将listen_fd上树
+    //将监听 sokcet 绑定到 epoll_fd 上去
 
-    epoll_event ev, evs[EvsSize];
+    epoll_event ev, evs[EpollEvsSize];
     ev.data.fd = listen_fd;
     ev.events = EPOLLIN;
-    epoll_ctl(epfd, EPOLL_CTL_ADD, listen_fd, &ev);
+
+    if(epoll_ctl(epoll_fd, EPOLL_CTL_ADD, listen_fd, &ev)==-1){
+        cerr << "epoll_ctl error" << endl;
+        close(listen_fd);
+        return -1;
+
+    };
+
+
     unordered_map<int, string> map;
+
     // while监听
     while (true) {
 
-        int n_ready = epoll_wait(epfd, evs, EvsSize, EpollWaitTimeout);
+        int n_ready = epoll_wait(epoll_fd, evs, EpollEvsSize, EpollWaitTimeout);
         cout << "epoll wait __________________________________________" << endl;
 
         if (n_ready < 0) {
-
+            //被信号中断
+            if (errno == EINTR)
+                continue;
             perror("epoll_wait");
             break;
-
         } else if (n_ready == 0) {
+            //超时,继续
             continue;
         } else {
             // 监听到了数据 有文件描述符变化
@@ -70,7 +84,6 @@ int main() {
 
 
                     // save client ip:port
-
                     char ip[INET_ADDRSTRLEN];
                     inet_ntop(AF_INET, &client_address.sin_addr.s_addr, ip, 16);
                     cout << "new client connect . . . and ip is " << ip << " port is : " <<
@@ -87,42 +100,40 @@ int main() {
                     fcntl(client_fd, F_SETFL, flag);
 
 
-
                     // 将 client_fd  上树
                     ev.data.fd = client_fd;
                     ev.events = EPOLLIN | EPOLLET;
-                    epoll_ctl(epfd, EPOLL_CTL_ADD, client_fd, &ev);
+                    epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_fd, &ev);
 
-
-                } else if (evs[i].events & EPOLLIN) {  // cfd变化 并且是读事件变化
+                }
+                // cfd变化 并且是读事件变化
+                else if (evs[i].events & EPOLLIN) {
                     while (true) {
-
-
                         ssize_t n;
                         char buffer[ReadBufferSize];
                         // 如果读一个缓冲区 缓冲区没有数据 如果是带阻塞 就阻塞等待
                         // 如果是非阻塞 返回值是-1 并将 erron 设置为 EINTR
                         n = read(evs[i].data.fd, buffer, ReadBufferSize);
                         if (n < 0) {
-
                             // 如果缓冲区读干净了 break 继续监听
+
                             if (errno == EAGAIN) {
-
                                 break;
-
                             }
-
-                            perror("read error");
-                            close(evs[i].data.fd);
-                            epoll_ctl(epfd, EPOLL_CTL_DEL, evs[i].data.fd, &evs[i]);
-
-                            break;
+                            else{
+                                //出错
+                                perror("read error");
+                                close(evs[i].data.fd);
+                                epoll_ctl(epoll_fd, EPOLL_CTL_DEL, evs[i].data.fd, &evs[i]);
+                                break;
+                            }
 
                         } else if (n == 0) {
 
+                            //对端关闭了连接，从 epoll_fd 上移除 client_fd
                             cout << "client [ " << map[evs[i].data.fd] << " ] aborted connection" << endl;
                             close(evs[i].data.fd);
-                            epoll_ctl(epfd, EPOLL_CTL_DEL, evs[i].data.fd, &evs[i]);
+                            epoll_ctl(epoll_fd, EPOLL_CTL_DEL, evs[i].data.fd, &evs[i]);
                             break;
                         } else {
 
@@ -130,21 +141,13 @@ int main() {
                             string str(buffer);
                             str = "server data : " + str;
                             Write(evs[i].data.fd, str.c_str(), str.size());
-                            // 没有使用buffer数据 所以需要
+                            // 没有使用buffer数据 所以需要memset()
                             memset(buffer, 0, sizeof(buffer));
-
-
                         }
                     }
-
                 }
-
             }
-
-
         }
-
-
     }
 
     return 0;
